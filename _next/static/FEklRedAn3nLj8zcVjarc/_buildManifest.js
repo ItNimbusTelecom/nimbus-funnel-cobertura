64105dab9959c81bb6a5610818214ace8c3eba51
Pablo Leone <info@tallerdedigitalizacion.com>
2026-06-29 14:23:00 +0200

    Updates

--- _next/static/FEklRedAn3nLj8zcVjarc/_buildManifest.js
+++ _next/static/FEklRedAn3nLj8zcVjarc/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/nimbus-funnel-cobertura//_next/:path+",
-        "destination": "/nimbus-funnel-cobertura/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
